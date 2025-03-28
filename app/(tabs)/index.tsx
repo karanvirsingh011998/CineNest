2af6d11d62596e831421ba7683b7b8e4dd93707e
karanvirsingh1998 <karanvirsingh@luminoguru.com>
2025-03-28 16:11:01 +0530

    logo updated and added in home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello, world!</Text>
+    <View className="flex-1 bg-primary">
+      <Image source={images.bg} className="absolute w-full z-0" />
+      <ScrollView className="flex-1 px-5">
+        <Image source={icons.logo} className="mt-5 mx-auto" />
+      </ScrollView>
     </View>
   );
 }
