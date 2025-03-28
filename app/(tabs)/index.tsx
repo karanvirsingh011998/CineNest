25ccda45e3528f4a9d35a6b55210349a96a39db9
karanvirsingh1998 <karanvirsingh@luminoguru.com>
2025-03-28 17:28:22 +0530

    logo updated and added in home page icon

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,10 +4,10 @@ import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 bg-primary">
+<View className="flex-1 bg-primary" >
       <Image source={images.bg} className="absolute w-full z-0" />
       <ScrollView className="flex-1 px-5">
-        <Image source={icons.logo} className="mt-5 mx-auto" />
+          <Image source={icons.logo} className="mt-5 mx-auto " style={{width: 100, height: 100}} />
       </ScrollView>
     </View>
   );
